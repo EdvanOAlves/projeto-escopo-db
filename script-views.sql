36c9c92e722935f80c860772fc6b7b01013a2e98
Edvan Alves <25132698@dsn127m1246731.cfp127aluno.local>
2026-04-30 11:22:24 -0300

    adiciona view de retorno de projetos com pfps

---
+++ script-views.sql
@@ -0,0 +1,17 @@
+
+CREATE VIEW vw_projetos_com_usuarios AS
+SELECT 
+	projeto.id, projeto.titulo, projeto.descricao, 
+	GROUP_CONCAT(usuario.foto_perfil) AS foto_usuarios
+FROM projeto
+JOIN usuario_projeto ON usuario_projeto.projeto_id = projeto.id
+JOIN usuario ON usuario_projeto.usuario_id = usuario.id
+GROUP BY projeto.id, projeto.titulo, projeto.descricao;
+
+-- Como usar a view
+SELECT * FROM vw_projetos_com_usuarios
+WHERE id IN(
+	SELECT projeto_id
+    FROM usuario_projeto
+    WHERE usuario_id = 2
+);
